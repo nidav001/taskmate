1ad2d9996d2d01a36ed95790107097f8c6d09687
nidav001 <niklas.davidsohn@gmail.com>
2022-12-01 21:40:52 +0100

    Logo as Link

--- src/components/shared/navigation/logo.tsx
+++ src/components/shared/navigation/logo.tsx
@@ -1,15 +1,18 @@
+import Link from "next/link";
 import classNames from "../../../utils/classNames";
 
 const Logo: React.FC<{ logoShown: string }> = ({ logoShown }) => {
   return (
-    <div
-      className={classNames(
-        logoShown,
-        "py-3 pl-1 text-2xl font-bold tracking-tight text-black"
-      )}
-    >
-      <span className="text-daccent">T3</span>Todo
-    </div>
+    <Link href="/">
+      <div
+        className={classNames(
+          logoShown,
+          "py-3 pl-1 text-2xl font-bold tracking-tight text-black"
+        )}
+      >
+        <span className="text-daccent">T3</span>Todo
+      </div>
+    </Link>
   );
 };
 
