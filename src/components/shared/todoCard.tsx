04fd911194b533e0f63856d05af7b7d811c80061
nidav001 <niklas.davidsohn@gmail.com>
2022-12-22 17:32:40 +0100

    isDragging style change

--- src/components/shared/todoCard.tsx
+++ src/components/shared/todoCard.tsx
@@ -38,7 +38,7 @@ function TodoCard({
       {...provided?.dragHandleProps}
       ref={provided?.innerRef}
       className={`group my-1 flex flex-col rounded-xl bg-gray-500 py-1 px-4 text-black hover:bg-gray-400 dark:bg-slate-500 dark:hover:bg-slate-600 ${
-        isDragging === undefined ? "dark:bg-red-200" : ""
+        isDragging === undefined ? "dark:bg-slate-300" : ""
       }`}
     >
       <div className="group flex items-center justify-between gap-2">
@@ -58,6 +58,7 @@ function TodoCard({
           }}
           defaultValue={todo.content}
           className={classNames(
+            isDragging === undefined ? "dark:bg-slate-300" : "",
             todoDone ? "line-through" : "",
             "resize-none border-0 bg-gray-300 text-base font-medium focus:ring-0 group-hover:bg-gray-400 dark:bg-slate-500 dark:group-hover:bg-slate-600"
           )}
